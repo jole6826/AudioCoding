c3c97695ff605cb9b2401b11b617bfa4020cce44
Johann Lembach <johannlembach@wl-l3-73.rz.tu-ilmenau.de>
2017-11-06 14:00:37 +0100

    test veraenderung

--- encframework.py
+++ encframework.py
@@ -6,6 +6,9 @@ import pickle
 # read wav segment
 [norm_audio, raw_audio, org_dtype] = basic_audio_proc.read_segment('Track48.wav', 8, 0);
 
+test = True;
+print test
+
 encoded16bit_audio = basic_audio_proc.quantize(norm_audio, org_dtype, 16);
 encoded8bit_audio = basic_audio_proc.quantize(norm_audio, org_dtype, 8);
 
