e2ae4085df7916059877ab1601a539cd595c95c0
Georg Götz <gorgi@wl-r8-136.rz.tu-ilmenau.de>
2017-11-07 15:00:16 +0100

    Added plotting and audio playback

--- decframework.py
+++ decframework.py
@@ -1,13 +1,19 @@
 import pickle
 import basic_audio_proc
 import numpy as np
+import matplotlib.pyplot as plt
 
-audio16bit = pickle.load(open('encoded16bit.bin', 'rb'));
-audio8bit = pickle.load(open('encoded8bit.bin', 'rb'));
+audio16bit = pickle.load(open('encoded16bit.bin', 'rb'))
+audio8bit = pickle.load(open('encoded8bit.bin', 'rb'))
 # 8 bit is unsigned: https://docs.scipy.org/doc/scipy-0.19.1/reference/generated/scipy.io.wavfile.write.html
-audio8bit = (audio8bit + 128).astype(np.uint8); 
+audio8bitUnsigned = (audio8bit + 128).astype(np.uint8)
 
-Print 'Test2';
+basic_audio_proc.write_wav('decoded16bit.wav', 44100, audio16bit)
+basic_audio_proc.write_wav('decoded8bit.wav', 44100, audio8bitUnsigned)
 
-basic_audio_proc.write_wav('decoded16bit.wav', 44100, audio16bit);
-basic_audio_proc.write_wav('decoded8bit.wav', 44100, audio8bit);
+[norm_audio, raw_audio, org_dtype, fs] = basic_audio_proc.read_segment('Track48.wav', 8, 1)
+
+plt.plot(basic_audio_proc.normalize(audio8bit), 'r', lw = 0.5, alpha = 0.5, label = '8 Bit Quantization')
+plt.plot(norm_audio, 'b', lw = 0.5, alpha = 0.5, label = 'Original wav')
+plt.legend()
+plt.show()
--- encframework.py
+++ encframework.py
@@ -2,15 +2,47 @@ import basic_audio_proc
 import numpy as np
 import warnings
 import pickle
+import matplotlib.pyplot as plt
+
+########################################
+# Adjust here before running the script#
+########################################
+channel = 1 # L = 0, R = 1
+length_segment = 8 # in seconds
+playplot_audio = False
+
 
 # read wav segment
-[norm_audio, raw_audio, org_dtype] = basic_audio_proc.read_segment('Track48.wav', 8, 0);
+[norm_audio, raw_audio, org_dtype, fs] = basic_audio_proc.read_segment('Track48.wav', length_segment, channel)
 
-test = True;
-Print 'Test1';
+encoded16bit_audio = basic_audio_proc.quantize(norm_audio, org_dtype, 16)
+encoded8bit_audio = basic_audio_proc.quantize(norm_audio, org_dtype, 8)
 
-encoded16bit_audio = basic_audio_proc.quantize(norm_audio, org_dtype, 16);
-encoded8bit_audio = basic_audio_proc.quantize(norm_audio, org_dtype, 8);
-
-pickle.dump(encoded16bit_audio, open('encoded16bit.bin', 'wb'), 1);
+pickle.dump(encoded16bit_audio, open('encoded16bit.bin', 'wb'), 1)
 pickle.dump(encoded8bit_audio, open('encoded8bit.bin', 'wb'), 1)
+   
+if playplot_audio:
+    plt.plot(norm_audio, lw = 0.2)
+    plt.show()
+    basic_audio_proc.play_audio(norm_audio, fs)
+    
+    plt.plot(encoded16bit_audio, lw = 0.2)
+    plt.show()    
+    basic_audio_proc.play_audio(encoded16bit_audio, fs)
+    
+    plt.plot(encoded8bit_audio, lw = 0.2)
+    plt.show()    
+    basic_audio_proc.play_audio(encoded8bit_audio, fs)
+    
+    # block into 1024 sample blocks
+    framed_audio = basic_audio_proc.frame_audio(norm_audio, 1024)
+    fft_audio = np.abs(np.fft.fft(framed_audio, axis=1))
+    frqz_resp = 20*np.log10(fft_audio)
+
+    plt.semilogx(frqz_resp[:,0], 'b', lw=0.4, alpha=0.5)
+    plt.semilogx(frqz_resp[:,1], 'r', lw=0.4, alpha=0.5)
+    plt.semilogx(frqz_resp[:,2], 'g', lw=0.4, alpha=0.5)
+    plt.semilogx(frqz_resp[:,3], 'y', lw=0.4, alpha=0.5)
+    plt.show()
+    
+    
