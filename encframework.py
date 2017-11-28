d6b8c8c71fbfc5b87a1977e4d36615eea55bb885
Georg Götz <gorgi@wl-r8-148.rz.tu-ilmenau.de>
2017-11-28 15:35:03 +0100

    Fixed dictionary lookup and added more structure to the code

--- encframework.py
+++ encframework.py
@@ -1,104 +1,35 @@
 import basic_audio_proc
 import huffmanCoding as hc
-import numpy as np
-import matplotlib.pyplot as plt
 import pickle
+import os
 
-########################################
-# Adjust here before running the script#
-########################################
-channel = 1 # L = 0, R = 1
-length_segment = 8 # in seconds
-plot_audio = False
-play_audio = False
 dumpFiles = True
-dumpHuffman = True
 
-
-# read wav segment
-[norm_audio, raw_audio, org_dtype, fs] = basic_audio_proc.read_segment('Track48.wav', length_segment, channel)
-
-# Quantize with 16 or 8 bit
-encoded16bit_audio = basic_audio_proc.quantize(norm_audio, org_dtype, 16)
-encoded8bit_audio = basic_audio_proc.quantize(norm_audio, org_dtype, 8)
-
-if dumpHuffman:
-    # create huffman codebook using signal
-    cb, cb_tree = hc.createHuffmanCodebook(encoded8bit_audio)
-    huffmanEncoded8bit_fullInfos = hc.huffmanEncoder(encoded8bit_audio, cb)
+def read_and_quantize(audio_id, length_segment, channel, n_bits):
+    # read wav segment
+    [norm_audio, raw_audio, org_dtype, fs] = basic_audio_proc.read_segment(audio_id, length_segment, channel)
     
-    ''' This results in filesize more than without Huffman...
-    n_samples = encoded8bit_audio.shape[0]
-    obj_to_pickle = dict({'tree': cb_tree, 'data': huffmanEncoded8bit_fullInfos.bin, 'nsamples': n_samples})
-    pickle.dump(obj_to_pickle, open('object.bin', 'wb'), 2)
-    '''
-    
-    ''' Another idea: create codebook using gaussian distribution of probablities
-    testVasls = np.linspace(-128,127,num=256)
-    mu = 0
-    sig = 30
-    x = np.linspace(0,256,num=256)
-    testHist = np.exp(-np.power(testVasls - mu, 2.) / (2 * np.power(sig, 2.)))
-    cbGaussianDist = hc.createHuffmanCodebookFromHist(testHist, testVasls)
-    
-    huffmanEncoded8bit_gaussianDist = hc.huffmanEncoder(encoded8bit_audio, cbGaussianDist)
-    '''
-    
-    # save Huffman encoded files as binary data
-    with open('encoded8bit_huffman.bin', 'wb') as f:
-        huffmanEncoded8bit_fullInfos.tofile(f)
-        f.close()
+    # Quantize with 16 or 8 bit
+    quantized_audio = basic_audio_proc.quantize(norm_audio, org_dtype, n_bits)
         
-    # codebook in extra file
-    pickle.dump(cb_tree, open('cb.bin', 'wb'), 1)
+    if dumpFiles:
+        dump_fname = 'encoded' + str(n_bits) + 'bit.bin'
+        pickle.dump(quantized_audio, open(os.path.join('bin', dump_fname), 'wb'), 1)
     
-    ''' Dump for Gaussian Distribution approach
-    with open('huffEncoded8bit_gaussianDist.bin', 'wb') as f:
-        huffmanEncoded8bit_gaussianDist.tofile(f)
-        f.close()
+    return quantized_audio, norm_audio, fs, dump_fname
+  
+def enc_huffman(audio):
+    cb, cb_tree = hc.createHuffmanCodebook(audio)
+    data_bits = hc.huffmanEncoder(audio, cb)
     
-    pickle.dump(cbGaussianDist, open('cbGaussianDist.bin', 'wb'), 1)
-    '''
-
-# save 8bit or 16bit as unencoded bitstream
-if dumpFiles:
-    pickle.dump(encoded16bit_audio, open('encoded16bit.bin', 'wb'), 1)
-    pickle.dump(encoded8bit_audio, open('encoded8bit.bin', 'wb'), 1)
-
-if play_audio:
-    print("Playing original:")
-    basic_audio_proc.play_audio(raw_audio, fs)
-    print("Playing 16bit quantized:")
-    basic_audio_proc.play_audio(encoded16bit_audio, fs)
-    print("Playing 8bit quantized:")
-    basic_audio_proc.play_audio(encoded8bit_audio, fs)
-
-if plot_audio:
-    t_axis = np.linspace(0, norm_audio.shape[0]-1, norm_audio.shape[0]) / float(fs)
-    plt.plot(t_axis, norm_audio, lw = 0.2)
-    plt.title('Original Audio')
-    plt.xlabel('t in seconds')
-    plt.show()
-
-    plt.plot(t_axis, encoded16bit_audio, lw = 0.2)
-    plt.title('16bit encoded Audio')
-    plt.xlabel('t in seconds')
-    plt.show()
-
-    plt.plot(t_axis, encoded8bit_audio, lw = 0.2)
-    plt.title('8bit encoded Audio')
-    plt.xlabel('t in seconds')
-    plt.show()
-
-    # block into 1024 sample blocks
-    framed_audio = basic_audio_proc.frame_audio(norm_audio, 1024)
-    fft_audio = np.abs(np.fft.fft(framed_audio, axis=0))
-    frqz_resp = 20*np.log10(fft_audio)
-
-    plt.plot(frqz_resp[0,:], 'b', lw=0.4, alpha=0.5)
-    plt.plot(frqz_resp[1,:], 'r', lw=0.4, alpha=0.5)
-    plt.plot(frqz_resp[2,:], 'g', lw=0.4, alpha=0.5)
-    plt.plot(frqz_resp[3,:], 'y', lw=0.4, alpha=0.5)
-    plt.xlabel('f in frequency bins')
-    plt.ylabel('Magnitude in dB')
-    plt.show()
+    n_zero_pad = 8 - ((len(data_bits)+3) % 8)
+    padded_bits = '{:b}'.format(n_zero_pad).zfill(3) + data_bits + '{:b}'.format(0).zfill(n_zero_pad)
+    data_binstring = hc.pack_bits_to_bytes(padded_bits)
+    
+    if dumpFiles:
+        dump_fname = 'encoded8bit_huffman.bin'
+        with open(os.path.join('bin', dump_fname), 'wb') as f:
+            pickle.dump(cb, f, 1)
+            pickle.dump(data_binstring, f, 1)   
+    
+    return cb, cb_tree, dump_fname
